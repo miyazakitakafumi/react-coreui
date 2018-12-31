3159c2b6ccbcd8f552b2989347b78c8bcb3dec82
TakafumiMiyazaki <takafumi.miyazaki@mt-square.co.jp>
2018-12-31 16:06:27 +0900

    styled-components 適用

--- src/views/Pages/Test/Test.js
+++ src/views/Pages/Test/Test.js
@@ -2,22 +2,30 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {Button, Col, Container, Row} from 'reactstrap'
 import {changeValue} from "../../../modules/test"
+import styled from 'styled-components'
+
+const ValueSpan = styled.span`
+    font-size: 2rem;
+    color: crimson;
+`
 
 class Test extends Component {
+
     render() {
         const {value} = this.props
-
         return (
             <div className="app flex-row align-items-center">
                 <Container>
                     <Row className="justify-content-center">
-                        <Col md='6'>
-                            {value}
+                        <Col md='6' className={'text-center'}>
+                            <ValueSpan>{value}</ValueSpan>
                         </Col>
                     </Row>
                     <Row className="justify-content-center">
-                        <Col md="6" className="">
+                        <Col md="6" className="text-center">
                             <Button color='primary' onClick={() => this.props.clickButton(1)}>+</Button>
+                        </Col>
+                        <Col md="6" className="text-center">
                             <Button color='danger' onClick={() => this.props.clickButton(-1)}>-</Button>
                         </Col>
                     </Row>
